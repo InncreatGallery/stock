54fe4a03e28cbb832630df06a25df9c5ce07a6cf
InncreatGallery <89318876+InncreatGallery@users.noreply.github.com>
2021-10-13 13:40:24 -0500

    Add files via upload

--- photography/image/css-js/js/script_footer.js
+++ photography/image/css-js/js/script_footer.js
@@ -22,72 +22,72 @@ var hipername;
 
 var facebook;
 
-	facebook = document.getElementById("facebook");
+	facebook = document.getElementById("slot1");
 	facebook.innerHTML = "Facebook";
 	facebook.href = "https://www.facebook.com/inncreat.inncreat";
 
 var pinterest;
 
-	pinterest = document.getElementById("pinterest");
+	pinterest = document.getElementById("slot2");
 	pinterest.innerHTML = "Pinterest";
 	pinterest.href = "https://pin.it/XO41nNu";
 
 var illustrations;
 
-	illustrations = document.getElementById("illustrations");
+	illustrations = document.getElementById("slot3");
 	illustrations.innerHTML = "Ilustraciones";
 	illustrations.href = "https://www.inncreat-gallery.ga/illustrations/index.html";
 
 var twiter;
 
-	twiter = document.getElementById("twiter");
+	twiter = document.getElementById("slot4");
 	twiter.innerHTML = "Twiter";
 	twiter.href = "https://twitter.com/inncreat";
 
 var images;
 
-	images = document.getElementById("images");
+	images = document.getElementById("slot5");
 	images.innerHTML = "Images";
 	images.href = "https://www.inncreat-gallery.ga/images/index.html";
 
 var videos;
 
-	videos = document.getElementById("videos");
+	videos = document.getElementById("slot6");
 	videos.innerHTML = "Videos";
 	videos.href = "https://www.inncreat-gallery.ga/videos/index.html";
 
 var instagram;
 
-	instagram = document.getElementById("instagram");
+	instagram = document.getElementById("slot7");
 	instagram.innerHTML = "Instagram";
 	instagram.href = "https://www.instagram.com/inncreat265/";
 
 var photography;
 
-	photography = document.getElementById("photography");
+	photography = document.getElementById("slot8");
 	photography.innerHTML = "Fotos";
 	photography.href = "https://www.inncreat-gallery.ga/photography/index.html";
 
 var sell;
 
-	sell = document.getElementById("sell");
+	sell = document.getElementById("slot9");
 	sell.innerHTML = "Vender";
 	sell.href = "https://www.inncreat.ga/menu/sell";
 
 var youtube;
 
-	youtube = document.getElementById("youtube");
+	youtube = document.getElementById("slot10");
 	youtube.innerHTML = "YouTube";
 	youtube.href = "https://www.youtube.com/channel/UCqNAPfHmIMJqDgJ7bb0_hyA";
 
 var vector;
 
-	vector = document.getElementById("vector");
+	vector = document.getElementById("slot11");
 	vector.innerHTML = "Vectores";
 	vector.href = "https://www.inncreat-gallery.ga/vector/index.html";
 
 var advertise;
 
-	advertise = document.getElementById("advertise");
+	advertise = document.getElementById("slot12");
 	advertise.innerHTML = "Anunciate";
 	advertise.href = "https://www.inncreat-gallery.ga/advertise/index.html";
