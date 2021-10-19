fb8b8f442dbac581706b182f5cefe22647b60743
InncreatGallery <89318876+InncreatGallery@users.noreply.github.com>
2021-10-19 14:02:00 -0500

    Add files via upload

---
+++ photography/css-js/js/script_footer.js
@@ -0,0 +1,93 @@
+var rights;
+
+	rights = document.getElementById("rights");
+	rights.innerHTML = "Todos los derechos reservados ";
+	rights.href = "https://www.inncreat.ga";
+
+var copyright;
+
+	copyright = document.getElementById("copyright");
+	copyright.innerHTML = "&#169;";
+
+var year;
+
+	year = document.getElementById("year");
+	year.innerHTML = "2021";
+	year.href = "https://www.inncreat.ga";
+
+var hipername;
+
+	hipername = document.getElementById("hipername");
+	hipername.innerHTML = "Inncreat";
+
+var facebook;
+
+	facebook = document.getElementById("slot1");
+	facebook.innerHTML = "Facebook";
+	facebook.href = "https://www.facebook.com/inncreat.inncreat";
+
+var pinterest;
+
+	pinterest = document.getElementById("slot2");
+	pinterest.innerHTML = "Pinterest";
+	pinterest.href = "https://pin.it/XO41nNu";
+
+var illustrations;
+
+	illustrations = document.getElementById("slot3");
+	illustrations.innerHTML = "Ilustraciones";
+	illustrations.href = "https://www.inncreat-gallery.ga/illustrations/index.html";
+
+var twiter;
+
+	twiter = document.getElementById("slot4");
+	twiter.innerHTML = "Twiter";
+	twiter.href = "https://twitter.com/inncreat";
+
+var images;
+
+	images = document.getElementById("slot5");
+	images.innerHTML = "Images";
+	images.href = "https://www.inncreat-gallery.ga/images/index.html";
+
+var videos;
+
+	videos = document.getElementById("slot6");
+	videos.innerHTML = "Videos";
+	videos.href = "https://www.inncreat-gallery.ga/videos/index.html";
+
+var instagram;
+
+	instagram = document.getElementById("slot7");
+	instagram.innerHTML = "Instagram";
+	instagram.href = "https://www.instagram.com/inncreat265/";
+
+var photography;
+
+	photography = document.getElementById("slot8");
+	photography.innerHTML = "Fotos";
+	photography.href = "https://www.inncreat-gallery.ga/photography/index.html";
+
+var sell;
+
+	sell = document.getElementById("slot9");
+	sell.innerHTML = "Vender";
+	sell.href = "https://www.inncreat.ga/menu/sell";
+
+var youtube;
+
+	youtube = document.getElementById("slot10");
+	youtube.innerHTML = "YouTube";
+	youtube.href = "https://www.youtube.com/channel/UCqNAPfHmIMJqDgJ7bb0_hyA";
+
+var vector;
+
+	vector = document.getElementById("slot11");
+	vector.innerHTML = "Vectores";
+	vector.href = "https://www.inncreat-gallery.ga/vector/index.html";
+
+var advertise;
+
+	advertise = document.getElementById("slot12");
+	advertise.innerHTML = "Anunciate";
+	advertise.href = "https://www.inncreat-gallery.ga/advertise/index.html";
---
+++ photography/css-js/js/script_header.js
@@ -0,0 +1,89 @@
+var head1;
+
+	head1 = document.getElementById("head1");
+	head1.href="https://inncreatblog.ga";
+	head1.innerHTML = "Home";
+
+var head2;
+
+	head2 = document.getElementById("head2");
+	head2.href="https://inncreat.ga/menu/html/advertise/inncreat";
+	head2.innerHTML = "Anunciate";
+
+var head3;
+
+	head3 = document.getElementById("head3");
+	head3.href="https://inncreat.ga/menu/html/sell/inncreat";
+	head3.innerHTML = "Subir";
+
+var head4;
+
+	head4 = document.getElementById("head4");
+	head4.href="https://www.facebook.com/inncreat.inncreat";
+	head4.innerHTML = "Contacto";
+
+var head5;
+
+	head5 = document.getElementById("head5");
+	head5.href="";
+	head5.innerHTML = "game over";
+
+var head6;
+
+	head6 = document.getElementById("head6");
+	head6.href="";
+	head6.innerHTML = "game over";
+
+var head7;
+
+	head7 = document.getElementById("head7");
+	head7.href="";
+	head7.innerHTML = "game over";
+
+var head8;
+
+	head8 = document.getElementById("head8");
+	head8.href="";
+	head8.innerHTML = "game over";
+
+//__________==========VISIVILIDAD==========__________
+
+var head_li1;
+
+	head_li1 = document.getElementById("head_li1");
+	head_li1.style.display = "flex";
+
+var head_li2;
+
+	head_li2 = document.getElementById("head_li2");
+	head_li2.style.display = "flex";
+
+var head_li3;
+
+	head_li3 = document.getElementById("head_li3");
+	head_li3.style.display = "flex";
+
+var head_li4;
+
+	head_li4 = document.getElementById("head_li4");
+	head_li4.style.display = "flex";
+
+var head_li5;
+
+	head_li5 = document.getElementById("head_li5");
+	head_li5.style.display = "none";
+
+var head_li6;
+
+	head_li6 = document.getElementById("head_li6");
+	head_li6.style.display = "none";
+
+var head_li7;
+
+	head_li7 = document.getElementById("head_li7");
+	head_li7.style.display = "none";
+
+var head_li8;
+
+	head_li8 = document.getElementById("head_li8");
+	head_li8.style.display = "none";
